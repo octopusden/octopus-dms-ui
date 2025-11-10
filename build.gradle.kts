2b20b8a0e51fcff8fdbf9d3ae1e9a2339ca5f94b
StanislavGro <stanislav.skii2000@mail.ru>
2025-11-10 22:51:27 +0300

    Added docker build image

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,105 @@
 import com.github.gradle.node.npm.task.NpmTask
+import java.time.Duration
 
 plugins {
+    signing
+    `maven-publish`
     kotlin("jvm") version "1.9.22"
+    id("org.springframework.boot") version "3.2.2"
     id("com.github.node-gradle.node") version "7.0.2"
+    id("com.bmuschko.docker-spring-boot-application") version "9.4.0"
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
 group = "org.octopusden.octopus.dms"
 version = "1.0-SNAPSHOT"
 
+fun String.getExt() = project.ext[this] as String
+
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://repo.gradle.org/gradle/libs-releases")
+    }
+}
+
+ext {
+    System.getenv().let {
+        set(
+            "signingRequired",
+            it.containsKey("ORG_GRADLE_PROJECT_signingKey") && it.containsKey("ORG_GRADLE_PROJECT_signingPassword")
+        )
+        set(
+            "dockerRegistry",
+            System.getenv().getOrDefault("DOCKER_REGISTRY", project.properties["docker.registry"])
+        )
+        set(
+            "octopusGithubDockerRegistry",
+            System.getenv()
+                .getOrDefault("OCTOPUS_GITHUB_DOCKER_REGISTRY", project.properties["octopus.github.docker.registry"])
+        )
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+            username.set(System.getenv("MAVEN_USERNAME"))
+            password.set(System.getenv("MAVEN_PASSWORD"))
+        }
+    }
+    transitionCheckOptions {
+        maxRetries.set(60)
+        delayBetween.set(Duration.ofSeconds(30))
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("bootJar") {
+            artifact(tasks.getByName("bootJar"))
+            from(components["java"])
+            pom {
+                name.set(project.name)
+                description.set("Octopus module: ${project.name}")
+                url.set("https://github.com/octopusden/octopus-dms-ui.git")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/octopus-dms-ui.git")
+                    connection.set("scm:git://github.com/octopusden/octopus-dms-ui.git")
+                }
+                developers {
+                    developer {
+                        id.set("octopus")
+                        name.set("octopus")
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    isRequired = project.ext["signingRequired"] as Boolean
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["bootJar"])
+}
+
+docker {
+    springBootApplication {
+        baseImage.set("${"dockerRegistry".getExt()}/eclipse-temurin:21-jdk")
+        ports.set(listOf(8080))
+        images.set(setOf("${"octopusGithubDockerRegistry".getExt()}/octopusden/${project.name}:${project.version}"))
+    }
 }
 
 val npmBuild = tasks.register<NpmTask>("npmBuild") {
