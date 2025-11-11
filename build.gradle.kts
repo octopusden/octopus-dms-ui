33b5535e533288643cad1ca741220f7498e42612
StanislavGro <stanislav.skii2000@mail.ru>
2025-11-11 14:35:22 +0300

    Added bootstrap config

--- build.gradle.kts
+++ build.gradle.kts
@@ -133,6 +133,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-devtools")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
 
     implementation("org.octopusden.octopus-cloud-commons:octopus-security-common:${project.properties["cloud-commons.version"]}") {
         exclude("org.springframework.security")
