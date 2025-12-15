96b2013f0f492051b1b2343f336749de68e1b480
Roman Ivanov <ivanov.roman.alexeevich@gmail.com>
2025-12-15 18:27:12 +0300

    Add back-channel logout

--- src/main/kotlin/org/octopusden/octopus/dms/configuration/SecurityConfig.kt
+++ src/main/kotlin/org/octopusden/octopus/dms/configuration/SecurityConfig.kt
@@ -3,13 +3,12 @@ package org.octopusden.octopus.dms.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
-import reactor.core.publisher.Mono
-import java.net.URI
 
 @Configuration
 @EnableWebFluxSecurity
@@ -20,36 +19,22 @@ open class SecurityConfig(
 
     @Bean
     open fun securityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
-        http
-            .authorizeExchange { exchanges ->
-                exchanges
-                    .pathMatchers(
-                        "/static/**",
-                        "/bundle.js",
-                        "/main.css",
-                        "/favicon.ico",
-                        "/logout",
-                        "/actuator/**",
-                    ).permitAll()
-                    .pathMatchers(
-                        "/",
-                        "/index.html",
-                        "/auth/**",
-                        "/rest/api/**",
-                    ).authenticated()
-                    .anyExchange().authenticated()
+        http.authorizeExchange { exchanges ->
+            exchanges.pathMatchers("/actuator/**").permitAll()
+            exchanges.anyExchange().authenticated()
+        }.oauth2Login(
+            Customizer.withDefaults()
+        ).oidcLogout{
+            it.backChannel(Customizer.withDefaults())
+        }.logout { logout ->
+            logout.logoutSuccessHandler { exchange, _ ->
+                exchange.exchange.response.apply {
+                    statusCode = HttpStatus.FOUND
+                    headers.add(HttpHeaders.LOCATION, logoutUrl)
+                }
+                exchange.exchange.session.flatMap { it.invalidate() }
             }
-            .oauth2Login(Customizer.withDefaults())
-            .logout { logout ->
-                logout
-                    .logoutSuccessHandler { webFilterExchange, _ ->
-                        val response = webFilterExchange.exchange.response
-                        response.statusCode = HttpStatus.FOUND
-                        response.headers.location = URI.create(logoutUrl)
-                        Mono.empty()
-                    }
-            }
-            .csrf { it.disable() }
+        }.csrf { it.disable() }
         return http.build()
     }
 }
